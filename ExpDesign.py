6d8a5106d4afbec492616f541e1a6cffc2725845
mawbray <maxmowbray@msn.com>
2020-03-12 10:07:22 +0000

    Reward Allocation Search

--- ExpDesign.py
+++ ExpDesign.py
@@ -10,8 +10,8 @@ from scipy.optimize import minimize
 eps  = np.finfo(float).eps
 import csv
 
-from Experiment import Experiment as ExpTrain
-from MonteCarloLearner import greedye_MCL, Greedye_MCLearned
+from Experiment_online import Experiment as ExpTrain
+from QLearnerOn import greedye_QL, Greedye_QLearned
 from Environment_Cont_x2_SIMO import Model_env
 from ValidationExperiment import Experiment_Done as ExpValid
 
@@ -20,16 +20,16 @@ from ValidationExperiment import Experiment_Done as ExpValid
 # Model definitions: parameters, steps, tf, x0  
       
 p        = {'u_m' : 0.0923*0.62, 'K_N' : 393.10, 'u_d' : 0.01, 'Y_nx' : 504.49}                 # model parameter definitions
-steps_   = np.array([10])                                                                       # number of control interactions
+steps_   = np.array([40])                                                                       # number of control interactions
 tf       = 16.*24                                                 
 x0       = np.array([0.5,150.0])                                                                # initial conditions of environment
-modulus  = np.array([0.05, 10])                                                                 # line distance of state space
+modulus  = np.array([0.025, 5])                                                                 # line distance of state space
 state_UB = np.array([5, 1000])                                                                  # state space upper bound
 
 # Agent definitions: num_actions, eps_prob, alpha, discount
 num_actions = 15                                                                                # number (range) of actions available to agent
-
-disc1 = np.array([0.10])                                                                        # discount factor in back-allocation
+alpha = 0.1
+disc1 = np.array([0.55])                                                                        # discount factor in back-allocation
 disc2 = np.array([0.99])                                                                        # discount factor in agent learning
 xi_ = np.array([3])                                                                             # Epsilon greedy definition (from experiment)
 
@@ -58,7 +58,7 @@ def Plotting(data, bracket, pNo_mean):
     plt.ylabel('Mean reward over ' + str(bracket)+ ' epochs', fontsize=28)
     #plt.title('Mean Reward over ' + str(bracket) + ' epochs with training')
     plt.tick_params(labelsize=24)
-    plt.savefig('D:\\Documents_Data\\UoM\\Projects\\First Year\\RL - Bioprocesses\\WorkBooks\\Reinforcement-Learning-for-Bioprocesses\\Accumulated_reward_' + str(pNo_mean) + 'MC.png')
+    plt.savefig('D:\\Documents_Data\\UoM\\Projects\\First Year\\RL - Bioprocesses\\WorkBooks\\Reinforcement-Learning-for-Bioprocesses\\Accumulated_reward_' + str(pNo_mean) + 'QLCAP_1_d055_d099_40steps_1e6.png')
 
 # running experiement
 for i in range(0, xi_.shape[0]):
@@ -66,10 +66,10 @@ for i in range(0, xi_.shape[0]):
     for k in range(0,disc2.shape[0]): 
       #run training 
       env = Model_env(p, steps_, tf, x0, modulus)                                               # calling environment
-      agent = greedye_MCL(num_actions, modulus, state_UB, disc1[j], disc2[k], steps_)           # calling agent
+      agent = greedye_QL(num_actions, modulus, state_UB, alpha, disc1[j], disc2[k], steps_)           # calling agent
       experiment = ExpTrain(env, agent, controls, episodes_train, xi_[i])                       # calling training experiment
       reward_training[:,i,j,k], d = experiment.simulation()                                     # running training experiment
-      agent = Greedye_MCLearned(num_actions, d, steps_)                                         # calling learned agent
+      agent = Greedye_QLearned(num_actions, d, steps_)                                         # calling learned agent
       exp_done = ExpValid(env, agent, controls, episodes_valid, 0)                              # calling validation experiment
       reward_validation[:,i,j,k] = exp_done.simulation()                                        # running validation experiment
       reward_train_mean = EpochNoMean(reward_training[:,i,j,k],bracket)
